868ec598daa9f3a03572483796df6591beaa53b6
tezov <tezov.app@gmail.com>
2023-11-09 20:24:18 +0100

    Initial Commit

---
+++ tabview/tabviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  tabviewApp.swift
+//  tabview
+//
+//  Created by prive on 09/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct tabviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
