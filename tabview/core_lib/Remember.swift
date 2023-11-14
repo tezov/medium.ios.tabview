c07fec2553aaf8448bc57e42a050913e4fe6918e
tezov <tezov.app@gmail.com>
2023-11-14 22:50:12 +0100

    improve lazyness of remember.

--- tabview/core_lib/Remember.swift
+++ tabview/core_lib/Remember.swift
@@ -1,21 +1,32 @@
 import SwiftUI
 
 @propertyWrapper public struct Remember<T: Any>: DynamicProperty {
-    @State @Lazy private var value: T
-
-    public var wrappedValue: T {
-        get { value }
-        nonmutating set { value = newValue }
+    @StateObject private var wrapper: Wrapper
+    
+    class Wrapper:ObservableObject {
+        var value:T
+        init(_ value: T) { self.value = value }
     }
-
+    
+    public var wrappedValue: T {
+        get { wrapper.value }
+        nonmutating set { wrapper.value = newValue }
+    }
+    
     public init(wrappedValue: @escaping @autoclosure () -> T) {
-        self._value = State(wrappedValue: Lazy(wrappedValue))
+        self._wrapper = .init(wrappedValue: Wrapper(wrappedValue()))
     }
     
     public init(_ value: @escaping () -> T) {
-        self._value = State(wrappedValue: Lazy(value))
+        self._wrapper = .init(wrappedValue: Wrapper(value()))
     }
     
+    public var projectedValue: Binding<T> {
+        Binding(
+            get: { wrappedValue },
+            set: { wrappedValue = $0 }
+        )
+    }
 }
 
 
